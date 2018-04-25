d09bb903bf464ee7078afee65b89624cd8172fb0
Yinchao <YinChaoOnline@users.noreply.github.com>
2018-04-25 10:54:58 +0800

    set cesium home button extent to HongKong

--- public/js/main.js
+++ public/js/main.js
@@ -10,3 +10,21 @@ const viewer = new Cesium.Viewer('cesiumContainer', {
     // baseLayerPicker: false,
     // terrainProvider: new EllipsoidTerrainProvider()
 });
+
+// set home button default view instead of flying to the world
+setHomeButtonView(Cesium);
+
+function setHomeButtonView(Cesium) {
+
+    // set home button default view
+    const west = 114.173746;
+    const south = 22.301674;
+    const east = 114.185974;
+    const north = 22.307335;
+
+    const rectangle = Cesium.Rectangle.fromDegrees(west, south, east, north);
+
+    //Cesium.Camera.DEFAULT_OFFSET = new Cesium.HeadingPitchRange(Cesium.Math.toRadians(295.04946043173993), Cesium.Math.toRadians(-44.81296187020527));
+    Cesium.Camera.DEFAULT_VIEW_FACTOR = 0;
+    Cesium.Camera.DEFAULT_VIEW_RECTANGLE = rectangle;
+}
